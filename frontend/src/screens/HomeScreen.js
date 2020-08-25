4b17a6d4c3435d51777d421696abc2d940aa0bf3
ariiyantto168 <ariiyantto168@gmail.com>
2020-08-25 14:45:09 +0700

    updated homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen(props) {
 
   // hooks
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/products");
+      setProduct(data);
+    }
+    return () => {
+      //
+    };
+  }, [])
+
     return <ul className="products">
     {/* di looping menggunakan map */}
     {
       // data.products di dapetin di data.js
-      data.products.map(product => 
+      products.map(product => 
         <li key={product._id}>
         <div className="product">
         <Link  to={'/product/' + product._id}>
