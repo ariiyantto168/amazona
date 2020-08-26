296f5507a8bc2769edc7eeffff4cc5a41c7a2384
ariiyantto168 <ariiyantto168@gmail.com>
2020-08-26 16:42:02 +0700

    updated reducers hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,4 +63,4 @@ function App() {
 
 export default App;
 
-// 1:56:57
+//2:20
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
-import { listProducts } from '../action/productActions';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen(props) {
 
